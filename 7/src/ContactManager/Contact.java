3f58551de339a65defff5129b28761ee5f013a10
sandeep-311 <2320030317@klh.edu.in>
2024-08-30 15:25:22 +0530

    Add files via upload

---
+++ 7/src/ContactManager/Contact.java
@@ -0,0 +1,29 @@
+package ContactManager;
+
+public class Contact implements Comparable<Contact>{
+    private final String name;
+    private final String email;
+    private final String phoneNumber;
+
+    public Contact(String name, String email, String phoneNumber){
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public int compareTo(Contact contact){
+        return name.compareTo(contact.getName());
+    }
+    public String toString(){
+        return "\nName: " + name + '\n' +
+                "Email: " + email + '\n' +
+                "Phone Number: " + phoneNumber;
+    }
+}
